556a79f8a8cc773d345c5a5a72ed4c6e529720e4
Gladys-m <gladysmitambo@gmail.com>
2023-10-20 12:44:13 +0400

    added portfolio components

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,12 +8,33 @@ import { styles } from '../styles';
 import { experiences } from '../constants';
 import { textVariant } from '../utils/motion';
 
+const ExperienceCard = ({experience}) => {
+  return (
+    <VerticalTimelineElement
+      contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+      contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+      date={experience.date}
+      iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    >
+       <h3 className="vertical-timeline-element-title">{experience.title}</h3>
+    </VerticalTimelineElement>
+  )
+}
+
 const Experience = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
         <h2 className={styles.sectionHeadText}>Work Experience</h2>
       </motion.div>
+
+      <div className='mt-20 flex flex-col'>
+        <VerticalTimeline>
+          {experiences.map((experience, index) => {
+            <ExperienceCard key={index} experience={experience}/>
+          })}
+        </VerticalTimeline>
+      </div>
     </>
   )
 }
